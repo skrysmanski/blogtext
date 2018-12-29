6a3e021d6b6ec67a896b74d0d59461170c713c10
Sebastian Krysmanski <noreply@manski.net>
2018-12-29 13:02:08 +0100

    Added stop script

---
+++ Stop-TestEnv.ps1
@@ -0,0 +1,37 @@
+#!/usr/bin/env pwsh
+param(
+    [string] $ProjectName = 'blogtext'
+)
+
+# Stop on every error
+$script:ErrorActionPreference = 'Stop'
+
+try {
+    & docker-compose --project-name $ProjectName down
+    if (-Not $?) {
+        throw '"docker-compose down" failed'
+    }
+}
+catch {
+    # IMPORTANT: We compare type names(!) here - not actual types. This is important because - for example -
+    #   the type 'Microsoft.PowerShell.Commands.WriteErrorException' is not always available (most likely
+    #   when Write-Error has never been called).
+    if ($_.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.WriteErrorException') {
+        # Print error messages (without stacktrace)
+        Write-Host -ForegroundColor Red $_.Exception.Message
+    }
+    else {
+        # Print proper exception message (including stack trace)
+        # NOTE: We can't create a catch block for "RuntimeException" as every exception
+        #   seems to be interpreted as RuntimeException.
+        if ($_.Exception.GetType().FullName -eq 'System.Management.Automation.RuntimeException') {
+            Write-Host -ForegroundColor Red $_.Exception.Message
+        }
+        else {
+            Write-Host -ForegroundColor Red "$($_.Exception.GetType().Name): $($_.Exception.Message)"
+        }
+        Write-Host -ForegroundColor Red $_.ScriptStackTrace
+    }
+
+    exit 1
+}
