0beea0a7787b23792d0743fbf2cf944d0b0fa542
Sebastian Krysmanski <noreply@manski.net>
2018-12-29 10:17:19 +0100

    First script for starting the test env

---
+++ Start-TestEnv.ps1
@@ -0,0 +1,57 @@
+#!/usr/bin/env pwsh
+param(
+    [int] $MaxConnectRetries = 20
+)
+
+# Stop on every error
+$script:ErrorActionPreference = 'Stop'
+
+try {
+    & docker-compose up --detach
+    if (-Not $?) {
+        throw '"docker-compose up" failed'
+    }
+
+    Write-Host
+    Write-Host 'Waiting for container to come up...'
+
+    for ($i = 0; $i -lt $MaxConnectRetries; $i++) {
+        try {
+            Invoke-WebRequest -Uri 'http://localhost:8080' | Out-Null
+            Write-Host -ForegroundColor Green 'Container is up'
+            break
+        }
+        catch {
+            if ($i -lt ($MaxConnectRetries - 1)) {
+                Start-Sleep -Seconds 3
+                Write-Host -ForegroundColor DarkGray "Attempt: $($i + 2)"
+            }
+            else {
+                Write-Error 'Container did not come up'
+            }
+        }
+    }
+}
+catch {
+    # IMPORTANT: We compare type names(!) here - not actual types. This is important because - for example -
+    #   the type 'Microsoft.PowerShell.Commands.WriteErrorException' is not always available (most likely
+    #   when Write-Error has never been called).
+    if ($_.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.WriteErrorException') {
+        # Print error messages (without stacktrace)
+        Write-Host -ForegroundColor Red $_.Exception.Message
+    }
+    else {
+        # Print proper exception message (including stack trace)
+        # NOTE: We can't create a catch block for "RuntimeException" as every exception
+        #   seems to be interpreted as RuntimeException.
+        if ($_.Exception.GetType().FullName -eq 'System.Management.Automation.RuntimeException') {
+            Write-Host -ForegroundColor Red $_.Exception.Message
+        }
+        else {
+            Write-Host -ForegroundColor Red "$($_.Exception.GetType().Name): $($_.Exception.Message)"
+        }
+        Write-Host -ForegroundColor Red $_.ScriptStackTrace
+    }
+
+    exit 1
+}
