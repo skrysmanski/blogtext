21ba81365076aa6ea865f007d2e2c787c0c55f2b
Sebastian Krysmanski <noreply@manski.net>
2021-09-25 11:47:03 +0200

    Updated catch block in Create-Release.ps1

--- Create-Release.ps1
+++ Create-Release.ps1
@@ -52,24 +52,42 @@ try {
     Get-ChildItem $DEST_DIR -Exclude '.svn' | Compress-Archive -DestinationPath "dist-zip/blogtext-$Version.zip"
 }
 catch {
+    function LogError([string] $exception) {
+        Write-Host -ForegroundColor Red $exception
+    }
+
+    # Type of $_: System.Management.Automation.ErrorRecord
+
+    # NOTE: According to https://docs.microsoft.com/en-us/powershell/scripting/developer/cmdlet/windows-powershell-error-records
+    #   we should always use '$_.ErrorDetails.Message' instead of '$_.Exception.Message' for displaying the message.
+    #   In fact, there are cases where '$_.ErrorDetails.Message' actually contains more/better information than '$_.Exception.Message'.
+    if ($_.ErrorDetails -And $_.ErrorDetails.Message) {
+        $unhandledExceptionMessage = $_.ErrorDetails.Message
+    }
+    elseif ($_.Exception -And $_.Exception.Message) {
+        $unhandledExceptionMessage = $_.Exception.Message
+    }
+    else {
+        $unhandledExceptionMessage = 'Could not determine error message from ErrorRecord'
+    }
+
     # IMPORTANT: We compare type names(!) here - not actual types. This is important because - for example -
     #   the type 'Microsoft.PowerShell.Commands.WriteErrorException' is not always available (most likely
     #   when Write-Error has never been called).
     if ($_.Exception.GetType().FullName -eq 'Microsoft.PowerShell.Commands.WriteErrorException') {
         # Print error messages (without stacktrace)
-        Write-Host -ForegroundColor Red $_.Exception.Message
+        LogError $unhandledExceptionMessage
     }
     else {
         # Print proper exception message (including stack trace)
         # NOTE: We can't create a catch block for "RuntimeException" as every exception
         #   seems to be interpreted as RuntimeException.
         if ($_.Exception.GetType().FullName -eq 'System.Management.Automation.RuntimeException') {
-            Write-Host -ForegroundColor Red $_.Exception.Message
+            LogError "$unhandledExceptionMessage$([Environment]::NewLine)$($_.ScriptStackTrace)"
         }
         else {
-            Write-Host -ForegroundColor Red "$($_.Exception.GetType().Name): $($_.Exception.Message)"
+            LogError "$($_.Exception.GetType().Name): $unhandledExceptionMessage$([Environment]::NewLine)$($_.ScriptStackTrace)"
         }
-        Write-Host -ForegroundColor Red $_.ScriptStackTrace
     }
 
     exit 1
